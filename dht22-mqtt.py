578c58bb30fa0b851036c57066d0ba19365e10ad
Farshid Tavakolizadeh <email@farshid.ws>
2022-03-31 14:47:14 +0200

    Refactor to use Paho's connection handling

--- dht22-mqtt.py
+++ dht22-mqtt.py
@@ -26,32 +26,41 @@ dhtDevice = adafruit_dht.DHT22(board.D4)
 
 mqtt_client=paho.Client("pluto-dht22")
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected: {}".format(paho.connack_string(rc)))
+
+def on_disconnect(client, userdata, rc):
+    print("Disonnected: {}".format(paho.connack_string(rc)))
+
+mqtt_client.on_connect = on_connect
+mqtt_client.on_disconnect = on_disconnect
+mqtt_client.connect_async(broker,port)
+mqtt_client.loop_start()
+
 while True:
-    mqtt_client.connect(broker,port)
+    try:
+        print("----------------------------")
+        temperature = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f}C  Humidity: {}% ".format(temperature, humidity))
 
-    while True:
-        try:
-            print("----------------------------")
-            temperature = dhtDevice.temperature
-            humidity = dhtDevice.humidity
-            print("Temp: {:.1f}C  Humidity: {}% ".format(temperature, humidity))
-
-            ret=mqtt_client.publish("pluto/dht22/temperature", temperature)
-            print("Published temperature. Client returned: {}".format(ret))
-
-            ret=mqtt_client.publish("pluto/dht22/humidity", humidity)
-            print("Published humidity. Client returned: {}".format(ret))
+        rc,_=mqtt_client.publish("pluto/dht22/temperature", temperature)
+        if rc != 0:
+            print("Publish temperature error: {}".format(paho.error_string(rc)))
+        
+        rc,_=mqtt_client.publish("pluto/dht22/humidity", humidity)
+        if rc != 0:
+            print("Publish humidity error: {}".format(paho.error_string(rc)))
 
 
-        except RuntimeError as error:
-            # Errors happen fairly often, DHT's are hard to read, just keep going
-            print(error.args[0])
-            time.sleep(2.0)
-            continue
-        except Exception as error:
-            dhtDevice.exit()
-            raise error
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        mqtt_client.loop_stop()
+        raise error
 
-        time.sleep(60.0)
-
-    time.sleep(10.0)
+    time.sleep(5.0)
