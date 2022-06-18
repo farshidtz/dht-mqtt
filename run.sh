4b51cd011d2052083b8c8624d4e89b1d150d5ca2
Farshid Tavakolizadeh <email@farshid.ws>
2022-06-18 17:06:49 +0200

    Add service file

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Load environment variables from env file
+set -o allexport
+source $DIR/config.env set
+set +o allexport
+
+python -u $DIR/dht22-mqtt.py
